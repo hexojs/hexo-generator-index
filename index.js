9c9dbfa6248f6fa3b403a69a96328cdfc0dde017
Boris Schapira <bschapira@clever-age.com>
2015-06-03 15:04:31 +0200

    fixes hexojs/hexo#1297

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var assign = require('object-assign');
 
 hexo.config.index_generator = assign({
-  per_page: hexo.config.per_page || 10
+  per_page: typeof hexo.config.per_page === "undefined" ? 10 : hexo.config.per_page
 }, hexo.config.index_generator);
 
 hexo.extend.generator.register('index', require('./lib/generator'));
