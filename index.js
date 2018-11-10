faa9485a81bebeef97e8088d5ad0320a5e394050
YoshinoriN <nayo-msoft@outlook.jp>
2018-11-11 05:05:40 +0900

    refactor(package): use Object.assign instead of object-assign dependency

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 'use strict';
 
-var assign = require('object-assign');
-
-hexo.config.index_generator = assign({
+hexo.config.index_generator = Object.assign({
   per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   order_by: '-date'
 }, hexo.config.index_generator);
